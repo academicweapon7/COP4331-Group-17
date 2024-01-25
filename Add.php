2c30575a73461fa4cdbd20288c1ceb1f81661680
Matthew McCullough <24968979+matthewsmccullough@users.noreply.github.com>
2024-01-25 00:20:40 -0500

    Add API

---
+++ Add.php
@@ -0,0 +1,36 @@
+<?php
+	$inData = getRequestInfo();
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,YachtName,YachtSize,Phone,Email,UserID) VALUES(?,?,?,?,?,?,?)");
+		$stmt->bind_param("sssissi", $inData["FirstName"], $inData["LastName"], $inData["YachtName"], $inData["YachtSize"], $inData["Phone"], $inData["Email"], $inData["ID"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
