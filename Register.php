5daf1a8dbdefa8261dd3bf882acdabaacfea4ff4
Matthew McCullough <24968979+matthewsmccullough@users.noreply.github.com>
2024-01-25 00:17:25 -0500

    Update Register.php

--- Register.php
+++ Register.php
@@ -2,11 +2,7 @@
 <?php
 
 	$inData = getRequestInfo();
-	
-	$ID = 0;
 	$Login = $inData["Login"];
-	$FirstName = "";
-	$LastName = "";
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
 	if( $conn->connect_error )
@@ -24,18 +20,18 @@
 		// Login already exists, return an error
 		if ( $row = $result->fetch_assoc())
 		{
-			returnWithError("Login is already in use.");
+			returnWithError( $Login, "Login is already in use.");
 		}
 		
 		// Add new user
 		else
 		{
-			$stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-			$stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Login"], $inData["Password"]);
+			$stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,YachtRegistration,Login,Password) VALUES(?,?,?,?,?)");
+			$stmt->bind_param("sssss", $inData["FirstName"], $inData["LastName"],  $inData["YachtRegistration"], $inData["Login"], $inData["Password"]);
 			$stmt->execute();
 			$stmt->close();
 			$conn->close();
-			returnWithError("");
+			returnWithInfo( $Login );
 		}
 	}
 	
@@ -50,15 +46,15 @@
 		echo $obj;
 	}
 	
-	function returnWithInfo( $FirstName, $LastName, $ID )
+	function returnWithInfo( $Login )
 	{
-		$retValue = '{"ID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
+		$retValue = '{"Login":"' . $Login . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
 	function returnWithError( $err )
 	{
-		$retValue = '{"ID":0,"Login":"' . $Login . '","error":"' . $err . '"}';
+		$retValue = '{"Login":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
